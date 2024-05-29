5d14d56ced426cb4b84ba748835b35c9cff03497
David Goraj <dg@netkitchen.de>
2024-05-29 15:31:10 +0200

    created script to measure moisture

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # Press Umschalt+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+import moistureMeasuring
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -15,3 +15,5 @@ if __name__ == '__main__':
     print_hi(input())
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+moistureMeasuring.measuring()
---
+++ moistureMeasuring.py
@@ -0,0 +1,7 @@
+# sensor = object
+
+moisture = 0
+def measuring():
+    if moisture < 10:
+        #watering plants
+        print("dirt humidity is under 10% watering plant ", 5, " activated")
