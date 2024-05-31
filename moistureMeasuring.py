fd465fa2e7c9b6fd8eb7368921c42a6566ce3109
David Goraj <dg@netkitchen.de>
2024-05-31 23:46:58 +0200

    modified moistureMeasuring with moisture limit

--- moistureMeasuring.py
+++ moistureMeasuring.py
@@ -1,7 +1,8 @@
 # sensor = object
+moisture_limit = 10
 
 moisture = 0
 def measuring():
-    if moisture < 10:
+    if moisture < moisture_limit:
         #watering plants
         print("dirt humidity is under 10% watering plant ", 5, " activated")
