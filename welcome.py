5226135d3d16651d1aa63703216df7e62660ef85
David Goraj <dg@netkitchen.de>
2024-06-25 16:03:32 +0200

    modified welcome script to work on Adding a user, user serialization is on hold

--- welcome.py
+++ welcome.py
@@ -1,23 +1,56 @@
+import json
+
+
 class User:
     def __init__(self, name, surname, email):
         self.name = name
         self.surname = surname
         self.email = email
 
+    def __str__(self):
+        return f'{"name":{self.name}, "surname": {self.surname}, "email": {self.email}}'
+
+    @classmethod
+    def new_user(cls):
+        # taking new user data by user input
+        print("Hey User type in your name:")
+        name = input()
+        print("Type your surname (optional)")
+        surname = input()
+        print("Type your email")
+        email = input()
+        return name, surname, email
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
 
+def create_user(mode):
+    # Use a breakpoint in the code line below to debug your script.
+    name, surname, email = User.new_user()
+    user = User(name, surname, email)
+    file = open("user.json", mode)
+    file.write(json.dumps(user)) #TODO: make user serializable to json
+    return user
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print("Hey User type in your name:")
-    print_hi(input())
+    try:
+        file = open("user.json", 'r')
+        user = file.read()
+        if len(user) == 0:
+            raise ValueError("User not set")
 
-try:
-    user = open("user.txt", 'r')
-except:
-    print("User file not exist. Creating new file.")
-    user = open("user.txt", 'x')
+    except FileNotFoundError:
+        print("User file not exist. \nCreating new file. \nCreating new User.")
+        user = create_user('x')
+
+    except ValueError:
+        print("User file exist, but empty. \nOverwriting file. \nCreating new User.")
+        user = create_user('w')
+
+
+    print_hi(user)
+
 
 
